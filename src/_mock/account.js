b36eb9580e68705c797b80f108e659560f3dd3f0
ashish4477 <ashishkrishu1725@gmail.com>
2024-03-30 17:59:16 +0530

    initial coomit

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Ashish kasodariya',
+  email: 'ashish.kasodariya@gmail.com',
+  photoURL: '/static/mock-images/avatars/avatar_6.jpg',
+};
+
+export default account;
